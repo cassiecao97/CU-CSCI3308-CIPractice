904813dfef2dded53c63b78abc3cf10bd035b5ab
Xinyu Cao <cassiecao2016@gmail.com>
2018-11-29 15:33:56 -0700

    coord_2d_area_triangle added

--- geometry_test.c
+++ geometry_test.c
@@ -145,6 +145,48 @@ START_TEST(test_2d_midpoint)
 }
 END_TEST
 
+START_TEST(test_2d_area_triangle)
+{
+    coord_2d_t a;
+    coord_2d_t b;
+    coord_2d_t c;
+
+    a.x = b.x = c.x = 0;
+    a.y = b.y = c.y = 0;
+    ck_assert(coord_2d_area_triangle(&a,&b,&c) == 0.0);
+
+    a.x = a.y = b.y = c.x = 0;
+    b.x = 1;
+    c.y = 1;
+    ck_assert(coord_2d_area_triangle(&a,&b,&c) == 0.5);
+
+    a.x = 2;
+    a.y = 1;
+    b.x = 3;
+    b.y = 6;
+    c.x = 4;
+    c.y = 4;
+    ck_assert(coord_2d_area_triangle(&a,&b,&c) == 3.5);
+
+    a.x = 5;
+    a.y = 10;
+    b.x = 15;
+    b.y = 25;
+    c.x = 20;
+    c.y = 15;
+    ck_assert(coord_2d_area_triangle(&a,&b,&c) == 87.5);
+
+    a.x = 15;
+    a.y = 30;
+    b.x = 20;
+    b.y = 45;
+    c.x = 40;
+    c.y = 10;
+    ck_assert(coord_2d_area_triangle(&a,&b,&c) == 237.5);
+}
+END_TEST
+
+
 /* coord_2d Test Suite */
 Suite* coord_2d_suite(void)
 {
@@ -162,10 +204,14 @@ Suite* coord_2d_suite(void)
     TCase* tc_2d_midpoint = tcase_create("coord_2d_midpoint");
     tcase_add_test(tc_2d_midpoint, test_2d_midpoint);
 
+    TCase* tc_2d_area_triangle = tcase_create("coord_2d_area_triangle");
+    tcase_add_test(tc_2d_area_triangle, test_2d_area_triangle);
+
     /* Add Cases to Suite */
     suite_add_tcase(s, tc_2d_eq);
     suite_add_tcase(s, tc_2d_dist);
     suite_add_tcase(s, tc_2d_midpoint);
+    suite_add_tcase(s, tc_2d_area_triangle);
 
     /* Return Suite */
     return s;
